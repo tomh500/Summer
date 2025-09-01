4404bdb012387ed28f656060f93dc35da05a4350
tomh500 <95513888+tomh500@users.noreply.github.com>
2025-09-01 22:56:26 +0800

    Delete open-source directory

--- open-source/小莫控制器/RTSSApi/rtss-core.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <string>
-#include <windows.h>
-
-void SetProperty(std::string profileStr, std::string propertyStr, int propValue);
-int GetProperty(std::string profileStr, std::string propertyStr);
-DWORD SetFlags(DWORD dwAND, DWORD dwXOR);
-DWORD GetFlags();
